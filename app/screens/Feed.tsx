ddc646fe1a88e483ce853c3111304ab72778805b
Sofia Dragomir <sofiateodora.dragomir@gmail.com>
2025-04-23 12:24:15 +0300

    change to feed

--- app/screens/Feed.tsx
+++ app/screens/Feed.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, StyleSheet, Image, Button } from 'react-native';
 import ItemEditScreen from "./ItemEdit";
 import ItemListScreen from "./ItemList";
 import MainLayout from "./Layout";
@@ -10,10 +11,27 @@ const ItemStack = createNativeStackNavigator();
 
 const ItemScreen = () => {
     return (
-        <MainLayout>
-            <Paragraph>da</Paragraph>
-       </MainLayout>
+        <View style={styles.container}>
+        <Text style={styles.title}>My Feed</Text>
+
+        </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 75,
+      paddingLeft: 10,
+      paddingRight:10,
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 20,
+      fontWeight: 'bold',
+    }
+  });
 export default ItemScreen;
