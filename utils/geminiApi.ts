209387a2fcf4394e57bac7552ef898fecab6143d
DorotheaP4 <157984051+DorotheaP4@users.noreply.github.com>
2025-04-30 10:05:39 +0300

    Update geminiApi.ts

--- utils/geminiApi.ts
+++ utils/geminiApi.ts
@@ -12,7 +12,7 @@ export async function fetchGeminiResponse(prompt: any) {
   };
 
   try {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${key}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
